2d75675b0a9680d787b628a1ae16de67f9ec0ad7
kane.woz <kane.woz@kakaocorp.com>
2023-02-03 13:41:23 +0900

    feat: Cookie 기능 구현

---
+++ src/main/java/webserver/cookie/CookieJar.java
@@ -0,0 +1,36 @@
+package webserver.cookie;
+
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+@NoArgsConstructor
+public class CookieJar {
+    private final Map<String, String> data = new HashMap<>();
+
+    public static CookieJar parse(String... rawDatas) {
+        var jar = new CookieJar();
+        for (var rawData : rawDatas) {
+            for (String each : rawData.split(";")) {
+                var splited = each.trim().split("=");
+                jar.add(splited[0], splited[1]);
+            }
+        }
+        return jar;
+    }
+
+    public Optional<String> add(String key, String value) {
+        return Optional.ofNullable(data.put(key, value));
+    }
+
+    public Set<String> keys() {
+        return data.keySet();
+    }
+
+    public Optional<String> get(String key) {
+        return Optional.ofNullable(data.getOrDefault(key, null));
+    }
+}
