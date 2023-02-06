41e5a26bb7c0abd234a4099dea29cb14a042c8c0
kane.woz <kane.woz@kakaocorp.com>
2023-02-06 13:54:33 +0900

    feat: request 분석 + index.html 핸들러 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,16 +3,14 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private String path;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -21,16 +19,37 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            parse(in);
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello world".getBytes();
+            // 분기
+            if (path.equals("/index.html")) {
+                responseIndexHtml(dos);
+                return;
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+
+    public void parse(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        var start = bufferedReader.readLine();
+        var startSplit = start.split(" ");
+        path = startSplit[1];
+    }
+
+    public void responseIndexHtml(DataOutputStream dos) throws IOException {
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/html;charset=utf-8 \r\n");
+        dos.writeBytes("Content-Length: 5 \r\n");
+        dos.writeBytes("\r\n");
+        dos.writeBytes("index");
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
