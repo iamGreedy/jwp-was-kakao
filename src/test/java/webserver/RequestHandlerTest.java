561f6b4745deaf929af16cc07e9caff6deffcb7b
kane.woz <kane.woz@kakaocorp.com>
2023-02-07 11:08:35 +0900

    test: /index.html 파일 분석

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -2,6 +2,11 @@ package webserver;
 
 import org.junit.jupiter.api.Test;
 import support.StubSocket;
+import utils.FileIoUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,31 +33,27 @@ class RequestHandlerTest {
         );
     }
 
-//    @Test
-//    void index() throws IOException, URISyntaxException {
-//        // given
-//        final String httpRequest = String.join("\r\n",
-//                "GET /index.html HTTP/1.1 ",
-//                "Host: localhost:8080 ",
-//                "Connection: keep-alive ",
-//                "",
-//                "");
-//
-//        final var socket = new StubSocket(httpRequest);
-//        final RequestHandler handler = new RequestHandler(socket);
-//
-//        // when
-//        handler.run();
-//
-//        // then
-//
-//
-//        var expected = "HTTP/1.1 200 \r\n" +
-//                "Content-Type: text/html;charset=utf-8 \r\n" +
-//                "Content-Length: 6902 \r\n" +
-//                "\r\n" +
-//                new String(FileIoUtils.loadFileFromClasspath("templates/index.html"));
-//
-//        assertThat(socket.output()).isEqualTo(expected);
-//    }
+    @Test
+    void index() {
+        // given
+        final var socket = new StubSocket(String.join("\r\n",
+                "GET /index.html HTTP/1.1",
+                ""
+        ));
+        final var server = WebServer.server();
+        // when
+        server.prepare(socket).run();
+        // then
+        try {
+            assertThat(socket.output().split("\r\n")).contains(
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html ",
+                    "Content-Length: 6902 ",
+                    "",
+                    new String(FileIoUtils.loadFileFromClasspath("templates/index.html"), StandardCharsets.UTF_8)
+            );
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
