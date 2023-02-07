116ca804ecac2eb5feadff790c3cbbfbd9df476e
kane.woz <kane.woz@kakaocorp.com>
2023-02-07 11:25:36 +0900

    test: 정적 리소스 확인을 위한 패러미터 테스트 작성

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -1,6 +1,8 @@
 package webserver;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import support.StubSocket;
 import utils.FileIoUtils;
 
@@ -33,27 +35,34 @@ class RequestHandlerTest {
         );
     }
 
-    @Test
-    void index() {
+    @ParameterizedTest
+    @CsvSource(delimiter = '|', value = {
+            "index.html             | text/html         | templates/index.html",
+            "user/login.html        | text/html         | templates/user/login.html",
+            "user/login_failed.html | text/html         | templates/user/login_failed.html",
+            "css/styles.css         | text/css          | static/css/styles.css",
+            "css/bootstrap.min.css  | text/css          | static/css/bootstrap.min.css",
+            "js/scripts.js          | text/javascript   | static/js/scripts.js",
+            "js/bootstrap.min.js    | text/javascript   | static/js/bootstrap.min.js",
+            "js/jquery-2.2.0.min.js | text/javascript   | static/js/jquery-2.2.0.min.js",
+    })
+    void staticResource(String path, String mime, String filepath) throws IOException, URISyntaxException {
         // given
         final var socket = new StubSocket(String.join("\r\n",
-                "GET /index.html HTTP/1.1",
+                String.format("GET %s HTTP/1.1", path),
                 ""
         ));
         final var server = WebServer.server();
         // when
         server.prepare(socket).run();
         // then
-        try {
-            assertThat(socket.output().split("\r\n")).contains(
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html ",
-                    "Content-Length: 6902 ",
-                    "",
-                    new String(FileIoUtils.loadFileFromClasspath("templates/index.html"), StandardCharsets.UTF_8)
-            );
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+        var file = FileIoUtils.loadFileFromClasspath(filepath);
+        assertThat(socket.output().split("\r\n")).contains(
+                "HTTP/1.1 200 OK ",
+                String.format("Content-Type: %s ", mime),
+                String.format("Content-Length: %s ", file.length),
+                "",
+                new String(file, StandardCharsets.UTF_8)
+        );
     }
 }
